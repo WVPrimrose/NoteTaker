8f7cfa34eefa5bf50ac2c84e578bdbe67f02f70b
Katie Buchholz <kzbindy@hotmail.com>
2024-05-24 11:17:43 -0400

    wrote the routes in the file; will check on it later to see if I did it right or not

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,3 +1,26 @@
 const router = require (express).router
 
-router.get
+router.get('/', (req, res) => {
+    console.log(`${req.method} requests for notes.html.`)
+    return res.json('notes.html')
+})
+
+router.get('*', (req, res) => {
+    console.log(`${req.method} requests for index.html.`)
+    return res.json('index.html')
+})
+
+router.post('./notes', (req, res) => {
+    console.log(`${req.method} requests to make a new note.`)
+    return res.json(`${req.method} sucessfully done!`)
+})
+
+router.delete('./notes/:id', (req, res) => {
+    console.log(`${req.method} requests to delete note.`)
+    return res.json(`${req.method} sucessfully done!`)
+})
+
+router.get('./', (req, res) => {
+    console.log(`${req.method} requests to get something.`)
+    return res.json(`${req.method} sucessfully done!`)
+})
