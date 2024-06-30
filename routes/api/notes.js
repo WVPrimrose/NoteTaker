8b1572fe1bf1b05fc4661bf94cd7d537bcdaf356
Katie Buchholz <kzbindy@hotmail.com>
2024-06-30 08:44:05 -0400

    trying to give the routes some functionality; particularly the api routes; also added a uuid js file to the repo, so notes can get their unique id note

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,19 +1,33 @@
 const router = require('express').Router()
+const notes = require('../../db/db.json')
+const fs = require('fs');
 
 // get requests
 router.get('/', (req, res) => {
     console.log(`${req.method} requests to get something.`)
-    return res.json(`${req.method} sucessfully done!`)
+    return res.json(`${req.method} sucessfully done!` (notes))
+
 })
 
 router.get ('/notes', (req, res) => {
     console.log(`${req.method} can read the note!`)
-    return res.json('Finally done!')
+    return res.json('Finally done!' (notes))
 })
 
 // post request
 router.post('/notes', (req, res) => {
     console.log(`${req.method} requests to make a new note.`)
+    const { title, text } = req.body;
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+        };
+    const response = {
+        status: 'success',
+        body: newNote,
+    };
+    }
     return res.json(`${req.method} sucessfully done!`)
 })
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const path = require('path');
-const apiNotes = require('./api/notes.js')
+const apiNotes = require('./api/notes.js');
+const notes = require('../db/db.json')
 
 router.get('/notes', (req, res) => {
     // console.log(`${req.method} requests for notes.html.`)
