c9e2e6dbc6ce45c57ae7dec8b3cca7a0effe85c8
Katie Buchholz <kzbindy@hotmail.com>
2024-07-01 11:59:15 -0400

    re-did the routes during my tutoring session; but trying to figure out how to get the post route to work; so far, it's not recognizing the req.body for whatever reason

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -2,35 +2,46 @@ const router = require('express').Router()
 const notes = require('../../db/db.json')
 const fs = require('fs');
 const uuid = require('../../helpers/uuid.js')
+const path = require('path');
 
-// get requests
-router.get('/', (req, res) => {
-    console.log(`${req.method} requests to get something.`)
-    return res.json(`${req.method} sucessfully done!` (notes))
-
-})
+// get request
 
 router.get ('/notes', (req, res) => {
     console.log(`${req.method} can read the note!`)
-    return res.json('Finally done!' (notes))
+    res.sendFile(path.join(__dirname, '../../db/db.json'));
+    // return res.json('Finally done!' (notes))
 })
 
 // post request
 router.post('/notes', (req, res) => {
     console.log(`${req.method} requests to make a new note.`)
-    const { title, text } = req.body;
-    if (title && text) {
-        const newNote = {
-            note_id: uuid(),
-            title,
-            text,
+    // const title = req.body.title;
+    // const text = req.body.text;
+    // if (title && text) {
+    //     const newNote = {
+    //         note_id: uuid(),
+    //         title,
+    //         text,
+    //     };
+    //     const noteString = JSON.stringify(newNote);
+    //     console.log(noteString);
+    // const response = {
+    //     status: 'success',
+    //     body: newNote,
+    // };
+    // }
+    let db = fs.readFileSync('db/db.json');
+        db = JSON.parse(db);
+        res.json(db);
+        console.log
+        let userNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: uuid(),
         };
-        const noteString = JSON.stringify(newNote);
-    const response = {
-        status: 'success',
-        body: newNote,
-    };
-    }
+        db.push(userNote);
+        fs.writeFileSync('db/db.json', JSON.stringify(db));
+        res.json(db);
     return res.json(`${req.method} sucessfully done!`)
 })
 
